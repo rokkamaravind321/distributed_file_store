ab85ed7a970d998fa75e13cd5577ca7586a5ddfc
Aravind Rokkam <aravindrokkam@Aravinds-MacBook-Air.local>
2025-04-21 15:53:45 -0700

    Initial commit: Basic P2P transport setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rokkamaravind321/dsitributed_file_store
+
+go 1.24.2
---
+++ p2p/transport.go
@@ -0,0 +1,9 @@
+package p2p
+
+// Interface that represent remote node(TCP,UDP,Websockets)
+type Peer interface {
+}
+
+// Handle comminucation between the nodes and the network
+type Transport interface {
+}
